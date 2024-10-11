81537786cb30a461e840f1904900d7aea69ac363
kely2822 <ferlysialily@gmail.com>
2024-10-11 18:25:09 +0700

    Pizza Management System Project Created

---
+++ src/main/java/com/mycompany/pizzamanagementsystem/PizzaManagementSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pizzamanagementsystem;
+
+/**
+ *
+ * @author ferlysia
+ */
+public class PizzaManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
